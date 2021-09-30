b0405806d31afcf101f18b5027d9f8742d1033d7
Luiz Carlos Froes <luizfroes@gmail.com>
2021-09-30 16:55:36 +0100

    solved

--- javascript/day-02/12-Stu_Arrays/Unsolved/script.js
+++ javascript/day-02/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,21 @@
 // WRITE YOUR CODE HERE
+// Declare the students array
+var students = ["Sarah", "Julia", "Henry", "Jack", "Bob"];
+
+//console.log the number of students
+console.log(students.length);
+
+//console.log message to each element of the array
+console.log("Welcome to the class " + students[0]);
+console.log("Welcome to the class " + students[1]);
+console.log("Welcome to the class " + students[2]);
+console.log("Welcome to the class " + students[3]);
+console.log("Welcome to the class " + students[4]);
+
+//replace the first element of the array
+students[0] = "Junior";
+
+//after confirm the change of the first student console.log message
+if (students[0] === "Junior") {
+  console.log(students[0] + " is in class");
+}
