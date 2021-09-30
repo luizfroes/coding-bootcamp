9b33db0b16060e3e8e61d69774a3b934fb2287ca
Luiz Carlos Froes <luizfroes@gmail.com>
2021-09-30 17:09:02 +0100

    solved

--- javascript/day-02/14-Stu_Iteration/Unsolved/script.js
+++ javascript/day-02/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,11 @@
 //WRITE YOUR CODE BELOW
+//create students array
+const students = ["Bob", "Julia", "Rick", "Thomas"];
+
+//console.log the number of students
+console.log(students.length);
+
+//use a for lup to greetings all students and console.log
+for (let i = 0; i < students.length; i++) {
+  console.log("Great to see you, " + students[i] + " !");
+}
