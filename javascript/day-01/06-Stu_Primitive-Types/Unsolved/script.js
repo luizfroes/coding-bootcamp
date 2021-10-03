da4c7988bb994e0b48ba126c3847ad353d72b669
Luiz Carlos Froes <luizfroes@gmail.com>
2021-10-03 21:58:03 +0100

    Resolved

--- javascript/day-01/06-Stu_Primitive-Types/Unsolved/script.js
+++ javascript/day-01/06-Stu_Primitive-Types/Unsolved/script.js
@@ -1,23 +1,41 @@
 var one = 64;
-var two = '656302';
+var two = "656302";
 var three = false;
 var four = 64.55;
-var five = 'Howdy!';
+var five = "Howdy!";
 var six;
 
 // Insert comments to explain what each console log below will log to the console
+
+//Number
 console.log(typeof one);
+
+//String
 console.log(typeof two);
+
+//Boolean
 console.log(typeof three);
+
+//Decimal Number
 console.log(typeof four);
+
+//String
 console.log(typeof five);
+
+//Undefined
 console.log(typeof six);
 
-four = 'Hello!';
+four = "Hello!";
 five = false;
 six = 23;
 
 // Insert comments to explain what each console log below will log to the console
+
+//String
 console.log(typeof four);
+
+//Boolean
 console.log(typeof five);
+
+//Number
 console.log(typeof six);
